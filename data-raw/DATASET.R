79f5c7277e5c93976e397b00959e894756eed8af
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-08-05 21:01:00 -0600

    Create dataset from CAPS data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -64,3 +64,44 @@ uci <- read_csv("data-raw/ucsb/go_track_trackspoints.csv") %>%
 #   addCircles()
 
 usethis::use_data(uci, overwrite = TRUE)
+
+
+
+# CAPS DATA (CONFIDENTIAL) ==============
+#
+# The file is really a folder that contains the trace information for
+# a single individual. Let's read all the CSV files in that folder
+files_in_folder <- dir("data-raw/caps/", full.names = TRUE)
+caps <- lapply(files_in_folder, function(x){
+  readr::read_csv(x, col_types = list(userId = col_character())) %>%
+    dplyr::transmute(
+      id = userId,
+      lat, lon,
+      timestamp,
+      date = lubridate::date(timestamp),   # Separate Date and Time columns
+      hour = lubridate::hour(timestamp),
+      minute = lubridate::minute(timestamp),
+      second = lubridate::second(timestamp),
+      time = hms::as_hms(str_c(hour, minute, second, sep = ":")),
+    ) %>% select(-hour, -minute, -second)
+}) %>%
+  dplyr::bind_rows()
+
+# find record with most data points
+ caps %>%
+   mutate(wday = wday(timestamp)) %>%
+   group_by(date(date)) %>%
+   summarise(n = n(), wday = wday[1], var = sd(lon)) %>% arrange(-n) %>%
+   View()
+
+caps_tr <- caps %>%
+  filter(date(date) == as_date("2021-03-05")) %>%
+  mutate(min = str_pad(str_c(hour(timestamp), minute(timestamp)), width = 4, pad = "0")) %>%
+  group_by(min) %>% slice_sample(n = 10) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4327) %>%
+  st_transform(32612)
+
+
+leaflet(caps_tr ) %>%
+  addProviderTiles(providers$OpenStreetMap) %>%
+  addCircleMarkers()
