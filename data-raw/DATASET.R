e4e67d5b61fc94b8cbd1ef3eb8ae16b37b4b8e2a
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-08-06 12:06:15 -0600

    Remove trajectory data
    The trajectories are only for trips and have activity locations censored. Want to get synthetic data to put in.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,66 +7,6 @@ library(dbscan)
 library(sf)
 
 
-# READ TRAJECTORIES ==================
-# Geolife trajectories https://www.microsoft.com/en-us/download/details.aspx?id=52367
-records <- str_pad(sample(0:181, 5), width = 3, pad =  "0")
-
-gps <- lapply(records, function(record){
-
-  plt_files <- dir(file.path("data-raw/geolife", record,
-                             "trajectory"), full.names = TRUE)
-
-  lapply(plt_files, function(file){
-    read_csv(file, skip = 6,
-             col_names = c("lat", "lng", "x", "altitude", "dumbdate",
-                           "date", "time"),
-             col_types = list(
-               date = col_date(),
-               time = col_time()
-             )) %>%
-      select(-x)
-
-  }) %>%
-    set_names(plt_files) %>%
-    bind_rows(.id = "file")
-
-})  %>%
-  set_names(records) %>%
-  bind_rows(.id = "record")
-
-# CLEAN TRAJECTORIES ==================
-trajectories <- gps %>%
-  # trajectories are in GMT; want to convert to local time
-  mutate(
-    gmt = as_datetime(str_c(date, time), tz = "GMT"),
-    localtime = with_tz(gmt, tz = "Asia/Hong_Kong"),
-    altitude = ifelse(altitude == -777, NA, altitude * 0.3048) # feet to meters
-  ) %>%
-  select(id = record, lat, lng, altitude, gmt, localtime) %>%
-
-  # make spatial and project into UTM 50N (meters)
-  st_as_sf(coords = c("lng", "lat"), crs = 4327) %>%
-  st_transform(32650)
-
-
-usethis::use_data(trajectories, overwrite = TRUE)
-
-
-#' UCI DATA ============
-
-uci <- read_csv("data-raw/ucsb/go_track_trackspoints.csv") %>%
-  filter(longitude > -37.2) %>%
-  filter(latitude < -10.8) %>%
-  st_as_sf(coords = c("longitude", "latitude"), crs = 4327)
-
-# leaflet(uci) %>%
-#   addProviderTiles(providers$OpenStreetMap) %>%
-#   addCircles()
-
-usethis::use_data(uci, overwrite = TRUE)
-
-
-
 # CAPS DATA (CONFIDENTIAL) ==============
 #
 # The file is really a folder that contains the trace information for
@@ -88,18 +28,18 @@ caps <- lapply(files_in_folder, function(x){
   dplyr::bind_rows()
 
 # find record with most data points
- caps %>%
-   mutate(wday = wday(timestamp)) %>%
-   group_by(date(date)) %>%
-   summarise(n = n(), wday = wday[1], var = sd(lon)) %>% arrange(-n) %>%
-   View()
+ # caps %>%
+ #   mutate(wday = wday(timestamp)) %>%
+ #   group_by(date(date)) %>%
+ #   summarise(n = n(), wday = wday[1], var = sd(lon)) %>% arrange(-n) %>%
+ #   View()
 
 caps_tr <- caps %>%
   filter(date(date) == as_date("2021-03-05")) %>%
   arrange(timestamp) %>%
   mutate(min = str_c(str_pad(hour(timestamp), width = 2, pad = "0"),
                      str_pad(minute(timestamp), width = 2, pad = "0"))) %>%
-  group_by(min) %>% slice_sample(n = 10) %>%
+  group_by(min) %>% slice_sample(n = 20) %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4327) %>%
   st_transform(32612)
 
